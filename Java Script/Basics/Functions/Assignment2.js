6e9f2a15108e77e00c271fb6c7592559d4067959
Nishant <iith.nishant@gmail.com>
2024-12-07 19:48:28 +0530

    DAY 2 JS DONE

--- Java Script/Basics/Functions/Assignment2.js
+++ Java Script/Basics/Functions/Assignment2.js
@@ -1,2 +1,10 @@
 // Calculate the time it takes between a setTimeout call and the inner function actully running
 
+// GPT 
+
+const startTime = performance.now();
+
+setTimeout(() => {
+    const endTime = performance.now();
+    console.log("Time elapsed:", (endTime - startTime).toFixed(2), "ms");
+}, 1000);
